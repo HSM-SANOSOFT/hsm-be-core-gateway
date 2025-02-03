e9a8c0300749b27c8b05ae4c6534c64e06f70b57
Raul Santamaria <raulsantamariao@hotmail.com>
2025-02-03 16:01:03 -0500

    modified:   src/has/gaa/gth/gsr/postulantes/postulantes.controller.ts

--- src/has/gaa/gth/gsr/postulantes/postulantes.controller.ts
+++ src/has/gaa/gth/gsr/postulantes/postulantes.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { envs } from 'config';
+import { catchError } from 'rxjs';
 
 @Controller()
 export class PostulantesController {
@@ -8,12 +9,20 @@ export class PostulantesController {
 
     @Get('/getPostulanteId/:CI')
     findOnePostulanteId(@Param('CI') CI: string) {
-        return this.client.send('getPostulanteId', {});
+        return this.client.send('getPostulanteId', {}).pipe(
+            catchError(err => {
+                throw new RpcException(err);
+            }),
+        );;
     }
 
     @Get('/getPostulante/:id')
     findOnePostulante(@Param('id') id: string) {
-        return this.client.send('getPostulante', { id });
+        return this.client.send('getPostulante', { id }).pipe(
+            catchError(err => {
+                throw new RpcException(err);
+            }),
+        );;
     }
 
 
