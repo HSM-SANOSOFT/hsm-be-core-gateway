083f72cd2ac564423f51671ba98f3018ec5e53c5
Raul Santamaria <raulsantamariao@hotmail.com>
2025-03-10 15:34:54 -0500

    Use void operator for bootstrap function call to indicate intentional disregard of the returned promise

--- src/main.ts
+++ src/main.ts
@@ -19,4 +19,4 @@ async function bootstrap() {
   await app.listen(envs.HSM_BE_CORE_GATEWAY_PORT);
   logger.log(`Gateway is active on port ${envs.HSM_BE_CORE_GATEWAY_PORT}`);
 }
-bootstrap();
+void bootstrap();
