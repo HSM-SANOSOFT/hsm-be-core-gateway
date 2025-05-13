cde6ff48757826f30ffc8c46b585685761d93d24
Raul Santamaria <raulsantamariao@hotmail.com>
2025-05-13 11:59:07 -0500

    Add chatbotMenus endpoint to CommonController

--- src/microservices/core/common/common.controller.ts
+++ src/microservices/core/common/common.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { s } from 'src/config';
 
 @Controller('common')
 export class CommonController {
   constructor(@Inject(s.hsm_be_core_common_name) private client: ClientProxy) {}
+
+  @Get('chatbotMenus/:id')
+  chatbotMenus(@Param('id') id: string) {
+    return this.client.send('chatbotMenus', { id: id });
+  }
 
   @Get('tipoAtencion')
   tipoAtencion() {
