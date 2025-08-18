267e6fe33d550e8f36ddcad0eaeca54d8638663e
Raul Santamaria <raulsantamariao@hotmail.com>
2025-08-18 15:53:06 -0500

    refactor: remove commented-out global prefix configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import { RequestMethod, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  /*app.setGlobalPrefix('api', {
-    exclude: [{ path: 'webhook/*path', method: RequestMethod.ALL }],
-  });*/
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
